65340a24a323b86a7f943fe030d53c7530b3f862
Bruce Hyslop <bruce.hyslop@csiro.au>
2022-03-10 17:25:41 +1100

    jwt verify based on algorithm from header (only RS256 & RS512 currently supported)

--- src/main/java/au/org/ala/ws/security/JwtUtils.java
+++ src/main/java/au/org/ala/ws/security/JwtUtils.java
@@ -3,15 +3,33 @@ package au.org.ala.ws.security;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.security.interfaces.RSAPublicKey;
 
 public final class JwtUtils {
 
+    static Logger log = LoggerFactory.getLogger(JwtUtils.class);
+
     private JwtUtils(){}
 
     public static boolean verify(RSAPublicKey publicKey, DecodedJWT jwt) throws SignatureVerificationException {
-        Algorithm algorithm = Algorithm.RSA256(publicKey, null);
+
+        Algorithm algorithm = null;
+
+        switch (jwt.getAlgorithm()) {
+            case "RS256":
+                algorithm = Algorithm.RSA256(publicKey, null);
+                break;
+            case "RS512":
+                algorithm = Algorithm.RSA512(publicKey, null);
+                break;
+            default:
+                log.warn("unsupported JWT algorithm {}", jwt.getAlgorithm());
+                return false;
+        }
+
         algorithm.verify(jwt);
         return true;
     }
